50cf53ea6cc376259cf65606412901eb1572ab6a
Sergey Yurkov <38420364+sergeyyurkov1@users.noreply.github.com>
2022-03-18 03:12:43 +0500

    Working sockets

--- app.py
+++ app.py
@@ -1,13 +1,33 @@
 from flask import Flask, request, render_template
 from markupsafe import escape
-import os
 
-app = Flask(__name__, template_folder="templates")
+flask_app = Flask(__name__, template_folder="templates")
 
-@app.route("/<app>", methods=["GET"])
+@flask_app.route("/<app>", methods=["GET"])
 def serve(app) :
     if request.method == "GET" :
-        return render_template( f"{escape(app)}.html" )
+        if not "." in app:
+            return render_template( f"{escape(app)}.html" )
+        else:
+            return f"{escape(app)}"
 
-# if __name__ == "__main__":
-#     app.run(debug=False)
+
+from flask_socketio import SocketIO, send, emit, join_room, leave_room
+
+socketio = SocketIO(flask_app)
+
+
+@socketio.on("json", namespace="/drawing")
+def handle_message(data):
+    global canvas
+    canvas = data
+    print(str(canvas))
+    emit("json", canvas, json=True, broadcast=True)
+
+@socketio.on("hello", namespace="/drawing")
+def handle_message(data):
+    print(data)
+    emit("json", canvas, json=True, broadcast=True)
+
+if __name__ == "__main__":
+    socketio.run(flask_app, host="0.0.0.0")
---
+++ static/drawing_for_two/sketch.js
@@ -0,0 +1,236 @@
+(function () {
+    var defaultOnTouchStartHandler = fabric.Canvas.prototype._onTouchStart;
+    fabric.util.object.extend(fabric.Canvas.prototype, {
+        _onTouchStart: function (e) {
+            var target = this.findTarget(e);
+            if (this.allowTouchScrolling && !target && !this.isDrawingMode) {
+                return;
+            }
+
+            defaultOnTouchStartHandler.call(this, e);
+        }
+    });
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+backgroundColor_ = "#e9e9ed";
+
+const appDimensions = () => {
+    const doc = document.documentElement;
+    doc.style.setProperty("--app-height", `${window.innerHeight}px`);
+    doc.style.setProperty("--app-width", `${window.innerWidth}px`);
+}
+window.addEventListener("resize", appDimensions);
+appDimensions();
+
+let weight = document.getElementById("weight");
+
+let c = document.getElementById("c");
+
+fabric.Object.prototype.transparentCorners = false;
+fabric.Object.prototype.selectable = false;
+
+var canvas = new fabric.Canvas("c", { isDrawingMode: true, width: 1000, height: 1000, allowTouchScrolling: true, backgroundColor: backgroundColor_ });
+
+canvas.selection = false;
+
+width = parseInt(document.getElementById("weight").value, 10) || 0;
+canvas.freeDrawingBrush.width = width;
+
+color = document.getElementById("color").value;
+canvas.freeDrawingBrush.color = color;
+
+document.getElementById("color").onchange = function () {
+    color = this.value;
+    canvas.freeDrawingBrush.color = color;
+};
+document.getElementById("weight").onchange = function () {
+    width = parseInt(this.value, 10) || 0;
+    canvas.freeDrawingBrush.width = width;
+};
+document.getElementById("clearButton").onclick = function () {
+    canvas.clear();
+    canvas.backgroundColor = backgroundColor_;
+    serializeCanvas();
+};
+
+document.getElementById("eraserButton").addEventListener("click", toggleEraser);
+function toggleEraser() {
+    // canvas.freeDrawingBrush = new fabric.EraserBrush(canvas);
+    canvas.freeDrawingBrush = new fabric.PencilBrush(canvas);
+    canvas.freeDrawingBrush.shadow = "none";
+    // canvas.freeDrawingBrush.inverted = true;
+    canvas.freeDrawingBrush.color = backgroundColor_;
+    canvas.freeDrawingBrush.width = width;
+
+    canvas.isDrawingMode = true;
+    isPanning = false;
+
+    ele.style.touchAction = "none";
+    ele.style.cursor = "auto !important"
+
+    ele.removeEventListener("mousedown", mouseDownHandler);
+}
+
+// panPan
+const ele = document.getElementById("canvasContainer");
+let pos = { top: 0, left: 0, x: 0, y: 0 };
+const mouseDownHandler = function (e) {
+    ele.style.cursor = "grabbing";
+    ele.style.userSelect = "none";
+
+    pos = {
+        left: ele.scrollLeft,
+        top: ele.scrollTop,
+        // Get the current mouse position
+        x: e.clientX,
+        y: e.clientY,
+    };
+
+    document.addEventListener("mousemove", mouseMoveHandler);
+    document.addEventListener("mouseup", mouseUpHandler);
+};
+const mouseMoveHandler = function (e) {
+    // How far the mouse has been moved
+    const dx = e.clientX - pos.x;
+    const dy = e.clientY - pos.y;
+
+    // Scroll the element
+    ele.scrollTop = pos.top - dy;
+    ele.scrollLeft = pos.left - dx;
+};
+const mouseUpHandler = function () {
+    ele.style.cursor = "grab";
+    ele.style.removeProperty("user-select");
+
+    document.removeEventListener("mousemove", mouseMoveHandler);
+    document.removeEventListener("mouseup", mouseUpHandler);
+};
+
+document.getElementById("pencilButton").addEventListener("click", togglePencil);
+function togglePencil() {
+    canvas.freeDrawingBrush = new fabric.PencilBrush(canvas);
+    canvas.freeDrawingBrush.shadow = "none";
+    // canvas.freeDrawingBrush.inverted = true;
+    canvas.freeDrawingBrush.color = color;
+    canvas.freeDrawingBrush.width = width;
+
+    canvas.isDrawingMode = true;
+    isPanning = false;
+
+    ele.style.touchAction = "none";
+    ele.style.cursor = "auto !important"
+
+    // ele.removeEventListener("mousedown", mouseDownHandler);
+}
+
+
+document.getElementById("panButton").addEventListener("click", togglePan);
+function togglePan() {
+    canvas.isDrawingMode = false;
+    isPanning = true;
+
+    ele.style.touchAction = "auto";
+    ele.style.cursor = "grab !important";
+
+    // ele.addEventListener("mousedown", mouseDownHandler);
+}
+
+
+const socket = io("/drawing");
+
+$(window).on("hidden.bs.modal", serializeCanvas);
+canvas.on("mouse:up", serializeCanvas);
+// canvas.on("mouse:move", serializeCanvas);
+// canvas.on("object:added", serializeCanvas);
+// canvas.on("object:modified", serializeCanvas);
+// canvas.on("object:removed", serializeCanvas);
+function serializeCanvas() {
+    // canvasJSON = canvas.toJSON();
+    canvasJSON = JSON.stringify(canvas);
+    // console.log(canvasJSON);
+    sendCanvas(canvasJSON);
+}
+
+function sendCanvas(canvasJSON) {
+    socket.emit("json", canvasJSON);
+    console.log("--> canvas sent");
+    receiveCanvas();
+}
+function receiveCanvas() {
+    socket.on("json", function (arg, callback) {
+        updateCanvas(arg);
+    });
+}
+function updateCanvas(newCanvas) {
+    console.log(newCanvas);
+    canvas.clear();
+    // setTimeout(console.log("Waiting..."), 5000);
+    // var canvas = new fabric.Canvas();
+    canvas.loadFromJSON(JSON.parse(newCanvas));
+    canvas.renderAll();
+    console.log("Done!");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(window).on("load", function () {
+    $("#exampleModal").modal("show"); // { "backdrop": "static", "keyboard": false, "show": true }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    //The first argument are the elements to which the plugin shall be initialized
+    //The second argument has to be at least a empty object or a object with your desired options
+    OverlayScrollbars(document.querySelectorAll("#canvasContainer"), { className: "os-theme-block-dark" });
+});
+
+$(window).on("shown.bs.modal", function () {
+    let uuid = self.crypto.randomUUID().split("-", 1);
+
+    $("#room-id").val(uuid).attr("readonly", "readonly");
+
+    // socket.on("connect", function () {
+    socket.emit("hello", "Connected!");
+    console.log("Connected!");
+    // });
+
+    document.getElementById("copyToClipboard").addEventListener("click", copyToClipboard);
+    function copyToClipboard() {
+        var room_id = document.getElementById("room-id");
+
+        room_id.select();
+        room_id.setSelectionRange(0, 99999); /* for mobile devices */
+
+        navigator.clipboard.writeText(room_id.value);
+    }
+
+});
